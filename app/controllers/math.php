95f5abb23350beb4ce2f03eff2d4424e6307b528
davidbrownucf <dcbrown07@gmail.com>
2014-03-11 17:07:52 -0400

    chaning name of myfn to math

--- app/controllers/math.php
+++ app/controllers/math.php
@@ -1,21 +1,25 @@
 <?php
 
-class myfn extends Core_Controller{
+class Math extends Core_Controller{
 	
 	function __construct(){
 		parent::__construct();
-		$this->debug('myfn ');
+		$this->debug('math fn ');
 	}
 	
 	function mymath(){
+
 		$this->debug('my math ');
+		$this->debug($this);
 		
 		$this->m = $this->model->load('math');
-
+		
+		$this->debug($this);
 		$data = array();
 		$data['mynum'] = $this->m->add(8,6);
 		$data['mynumsub'] = $this->m->subtract(8,4);
-		
+		print_r($data);
+		$this->debug('my math ');
 		$this->view->load('calcout',$data);
 	}
 	
